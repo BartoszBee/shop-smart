edb13282c43dfa9908fd7e187e9541e681f858e8
bartoszb <b.biernacki@delta.poznan.pl>
2025-09-05 13:09:49 +0200

    list of products maped

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { ShoppingCart } from "lucide-react";
-import {PRODUCTS} from "../data/products";
-import ProductCard from '../components/ProductCard'
-
-const product1 = PRODUCTS[0];
+import { PRODUCTS } from "../data/products";
+import ProductCard from "../components/ProductCard";
 
 export default function App() {
+  const handleAddToCart = (product: (typeof PRODUCTS)[number]) => {
+    console.log("[ADD_TO_CART]", product.id, product.name);
+  };
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
       <header className="bg-blue-600 text-white p-4 shadow">
@@ -16,14 +17,21 @@ export default function App() {
       <main className="p-4 grid grid-cols-4 gap-4">
         <section className="col-span-3 bg-white p-4 rounded shadow">
           <h2 className="text-xl font-semibold mb-4">Produkty</h2>
-          <p className="text-gray-600">Tutaj pojawi się lista produktów</p>
+          <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3">
+            {PRODUCTS.map((p) => (
+              <ProductCard
+                key={p.id}
+                product={p}
+                onAddToCart={handleAddToCart}
+              />
+            ))}
+          </div>
         </section>
         <aside className="col-span-1 bg-white p-4 rounded shadow">
           <h2 className="text-xl font-semibold mb-4">Koszyk</h2>
           <p className="text-gray-600">Koszyk jest pusty</p>
         </aside>
       </main>
-      <ProductCard product={product1} />
     </div>
   );
 }
