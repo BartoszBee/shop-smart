82b4db848968f35a0f15dcb2df959250fae38f12
bartoszb <b.biernacki@delta.poznan.pl>
2025-09-08 13:12:51 +0200

    vitest + rtl added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwind()],
+  test: {
+    globals: true,             // pozwala pisać testy bez importowania describe/it/expect
+    environment: "jsdom",      // DOM w Node
+    setupFiles: "./vitest.setup.ts", // globalny setup (matchery RTL)
+  },
 });
